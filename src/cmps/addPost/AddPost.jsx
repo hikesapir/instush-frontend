06511e4e45b03cea7c6baa7c61e1ced7a7a5db81
sapir hiki <sapirhiki@gmail.com>
2022-06-04 15:54:52 +0300

    started adding post details

--- src/cmps/addPost/AddPost.jsx
+++ src/cmps/addPost/AddPost.jsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { ReactComponent as CloseIcon } from '../../assets/icons/close-icon.svg'
 import { ReactComponent as MediaIcon } from '../../assets/icons/media-icon.svg';
+import { ReactComponent as BackIcon } from '../../assets/icons/back-icon.svg';
 import { uploadImg } from '../../services/img-upload.service';
 import { ImgEditor } from './ImgEditor';
+import { useSelector } from 'react-redux';
 
 
 
 export const AddPost = ({ closeModal }) => {
     const [image, setImage] = useState(null)
     const modalRef = useRef(null)
-
-    // useEffect(() => {
-    //     console.log(modalRef.current.style.width);
-
-    // }, [modalRef])
+    const { loggedinUser } = useSelector(state => state.userModule)
 
     const handleFile = async (ev) => {
         ev.stopPropagation();
@@ -46,12 +44,30 @@ export const AddPost = ({ closeModal }) => {
                 <CloseIcon />
             </span>
             <div className='modal' ref={modalRef}>
-                <header className='bold'>
-                    <div>Create new post</div>
+                {image ? <header className='bold'>
+                    <div><button onClick={() => setImage(null)}><BackIcon /></button> <h1 className='bold' >Corp</h1> <button className='bold'>Next</button></div>
+
                 </header>
+                    : <header className='bold'>
+                        <div>Create new post</div>
+                    </header>}
                 <section className='content'>
                     {image ?
-                        <ImgEditor image={image} />
+                        <section className='img-editor'>
+                            <ImgEditor image={image} />
+                            <div className="img-details">
+                                <header>
+                                    <div className='user-preview'>
+                                        <img className='img-medium' src={loggedinUser.imgUrl} alt="" />
+                                        <div>
+                                            <p className='bold'>{loggedinUser.username}</p>
+                                        </div>
+                                    </div>
+                                </header>
+                                <textarea type='input' placeholder="Write your caption..." autoComplete="off" autoCorrect="off" name='txt'></textarea>
+                                <input type="text" placeholder="addd location" />
+                            </div>
+                        </section>
                         : <main onDragOver={preventDefault}
                             onDrop={handleFile} >
                             <MediaIcon></MediaIcon>
