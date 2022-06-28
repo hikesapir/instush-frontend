aa6eafb4013e4526ed3c64ddf6f013a265805b49
sapir hiki <sapirhiki@gmail.com>
2022-06-28 09:09:48 +0300

    change files name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './assets/scss/style.scss'
 import { MainApp } from './pages/MainApp';
 import { AppHeader } from './cmps/AppHeader';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import { UserFeed } from './pages/UserFeed';
+import { UserProfile } from './pages/UserProfile';
 import { loadLoggedinUser } from './store/actions/userActions';
 import { loadPosts } from './store/actions/postActions';
 import { PostDetails } from './pages/PostDetails';
@@ -23,7 +23,7 @@ function App() {
         <AppHeader></AppHeader>
         <Switch>
           <Route path="/post/:postId" component={PostDetails}></Route>
-          <Route path="/feed/:userId" component={UserFeed}></Route>
+          <Route path="/feed/:userId" component={UserProfile}></Route>
           <Route path="/" component={MainApp}></Route>
         </Switch>
       </div>
