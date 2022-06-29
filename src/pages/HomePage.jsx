42330ba8c8b5a4fded8964c5816fb6f5ff11ca1d
sapir hiki <sapirhiki@gmail.com>
2022-06-29 20:36:27 +0300

    full homepage design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,9 @@ import img1 from '../assets/img/1.png'
 import img2 from '../assets/img/2.png'
 import img3 from '../assets/img/3.png'
 import img4 from '../assets/img/4.png'
+import appStore from '../assets/img/app-store.png'
+import googlePlay from '../assets/img/google-play.png'
+
 
 export const HomePage = () => {
     return (
@@ -12,11 +15,11 @@ export const HomePage = () => {
                     {[img1, img2, img3, img4].map(img => <img src={img} alt="" />)}
                 </div>
                 <div className="login-container">
-                    <div className="login">
+                    <div className="login container">
                         <div className='logo' >Instush</div>
                         <form className='register' action="">
                             <input type="text" autocomplete="off" name="username" placeholder="Username" />
-                            <input type="text" />
+                            <input type="password"  autocomplete="off" name="password" placeholder="Password" />
                             <button className='log-in-btn btn-primary' disabled>Log In</button>
                         </form>
                         <div className="space">
@@ -30,8 +33,18 @@ export const HomePage = () => {
                         </div>
                         <div className='dark-blue-txt'>Forgot password?</div>
                     </div>
-                    <div className="sign-up"></div>
-                    <div className="get-app"></div>
+                    <div className="sign-up container">
+                        <p> Don't have an account?
+                            <a href="#/homepage" className='primary-txt'> Sign up</a>
+                        </p>
+                    </div>
+                    <div className="get-app">
+                        <p>Get the app.</p>
+                        <div className='download-buttons-container'>
+                            <img src={appStore} alt="" />
+                            <img src={googlePlay} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
