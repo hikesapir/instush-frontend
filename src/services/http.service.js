e7fae364f36b836c4842f7b4da8f37ee98f26ee2
sapir hiki <98388195+hikesapir@users.noreply.github.com>
2022-10-22 18:11:19 +0300

    Update http.service.js

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,9 +1,11 @@
 import Axios from 'axios'
 // import  router  from '../router'
 
-const BASE_URL = process.env.NODE_ENV === 'production'
-    ? 'https://insta-api-1mdh.onrender.com/api/'
-    : '//localhost:3030/api/'
+// const BASE_URL = process.env.NODE_ENV === 'production'
+//     ? 'https://insta-api-1mdh.onrender.com/api/'
+//     : '//localhost:3030/api/'
+
+const BASE_URL = '//localhost:3030/api/'
 // const BASE_URL = process.env.NODE_ENV === 'production'
 //     ? '/api/'
 //     : '//localhost:3030/api/'
