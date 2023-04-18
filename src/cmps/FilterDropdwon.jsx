9261c0394e54c20510faaf8f38f025a7a3428428
sapir hiki <sapirhiki@gmail.com>
2023-04-18 21:25:27 +0300

    fixed colse when clicked outside filter dropdown

--- src/cmps/FilterDropdwon.jsx
+++ src/cmps/FilterDropdwon.jsx
@@ -1,25 +1,27 @@
 import React, { useRef, useState } from 'react'
 import { useEffect } from 'react';
 import { UserPreview } from './UserPreview';
+import { useOnClickOutside } from '../hooks/useOnClickOutside';
 
 export const FilterDropdwon = ({ users, isOpen, setIsOpen }) => {
   const [className, setClassName] = useState('hide')
   const dropdwon = useRef(null)
+  useOnClickOutside(dropdwon, () => setIsOpen(false))
 
-  useEffect(() => {
-    document.addEventListener("mousedown", (ev) => {
-      if (!ev.path.includes(dropdwon.current)) {
-        setIsOpen(false)
-      }
-    })
-  })
+  // useEffect(() => {
+  //   document.addEventListener("mousedown", (ev) => {
+  //     if (!ev.path.includes(dropdwon.current)) {
+  //       setIsOpen(false)
+  //     }
+  //   })
+  // })
 
   useEffect(() => {
     setClassName(isOpen ? '' : 'hide')
 
   }, [isOpen])
 
-
+  window.scrollTo(0, 0)
 
   return (
     <section className='filter-dropdwon'>
@@ -30,7 +32,7 @@ export const FilterDropdwon = ({ users, isOpen, setIsOpen }) => {
           <ul>
             {users?.map(user => {
               return (
-                <li key={user._id}>
+                <li key={user._id} onClick={() => setIsOpen(false)}>
                   <UserPreview user={user}></UserPreview>
                 </li>
               )
